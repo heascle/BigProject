97ea28cef92b60fd825a04c2672c518b72e51ad5
heascle <leo_heascle@foxmail.com>
2018-04-11 15:14:20 +0800

    Create Banner.java

---
+++ BurpSuitExtender/src/turnitup/utils/Banner.java
@@ -0,0 +1,13 @@
+package turnitup.utils;
+
+public class Banner {
+    public static String Author() {
+	//this.extPrint.println("-----------------------------------");
+	//this.extPrint.println("-------Gatherer 1.0.0-------");
+	//this.extPrint.println("----------Author:Turn it up--------");
+	//this.extPrint.println("-----------------------------------");
+	String a="1";
+	a.repea
+	return "-".repeat(3);
+    }
+}
